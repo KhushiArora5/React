a7b360821eaf3186ebc7965488d5ff1252f221d1
Khushi Arora <khushiarora752002@gmail.com>
2022-07-14 23:35:35 +0530

    changes

--- src/mango.js
+++ src/mango.js
@@ -2,9 +2,38 @@ import React from "react";
 
 export default function Mango()
 {
+    const products = [
+        {
+            productName : "iphone",
+            quantity : 0,
+        },
+        {
+            productName : "dell",
+            quantity : 0,
+        },
+        {
+            productName : "lenovo",
+            quantity : 0,
+        },
+        {
+            productName : "apple",
+            quantity : 0,
+        },
+        {
+            productName : "hp",
+            quantity : 0,
+        },
+    ]
     return(
-        <div>
-            <div className="text-center">Mango</div>
+        <div className="text-center">
+            {products.map((product) => {
+                return(
+                    <div>
+                        <div>Product Name : {product.productName}</div>
+                        <div>Product Qunatity : {product.quantity}</div>
+                    </div>
+                );
+            })}
         </div>    
     )
 }
